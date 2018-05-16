66170fff37e411b108e033f4766cf1eda8bcbff6
parkito <artyom-karnov@yandex.ru>
2018-05-16 16:47:31 +0300

    Fix Dockerfile

--- Hello_Docker/HelloJava.java
+++ Hello_Docker/HelloJava.java
@@ -2,7 +2,7 @@ public class HelloJava {
 
      public static void main(String... args) {
          for(int i = 0;i < 10; i++){
-	     System.out.println("Hello from java!");
+	     System.out.println("Hello from Java!");
 	 }
      }
 }
